604a73dd5e008b800828210b0ec17f80cff5bf0e
Arne Hitzmann <arne.hitzmann@gmail.com>
2020-04-01 01:15:39 +0900

    added venv to repository

--- corona_stats.py
+++ corona_stats.py
@@ -9,18 +9,18 @@ DEFAULT_CONFIRMED_PATH = 'https://github.com/CSSEGISandData/COVID-19/blob/master
 DEFAULT_DEATHS_PATH = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 DEFAULT_RECOVERED_PATH = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
 
-CONFIRMED_PATH = get_file_from_link(DEFAULT_CONFIRMED_PATH)
-DEATH_PATH = get_file_from_link(DEFAULT_DEATHS_PATH)
-RECOVERED_PATH = get_file_from_link(DEFAULT_RECOVERED_PATH)
+CONFIRMED_FILE = get_file_from_link(DEFAULT_CONFIRMED_PATH)
+DEATH_FILE = get_file_from_link(DEFAULT_DEATHS_PATH)
+RECOVERED_FILE = get_file_from_link(DEFAULT_RECOVERED_PATH)
 
-CONFIRMED_DATA, DATE_FIELDS = sanitize_data(pd.read_csv(CONFIRMED_PATH))
-DEATH_DATA, DATE_FIELDS = sanitize_data(pd.read_csv(DEATH_PATH))
-REOVERED_DATA, DATE_FIELDS = sanitize_data(pd.read_csv(RECOVERED_PATH))
+CONFIRMED_DATA, DATE_FIELDS = sanitize_data(pd.read_csv(CONFIRMED_FILE))
+DEATH_DATA, DATE_FIELDS = sanitize_data(pd.read_csv(DEATH_FILE))
+REOVERED_DATA, DATE_FIELDS = sanitize_data(pd.read_csv(RECOVERED_FILE))
 
 
 def main():
     # Show single Country
-    show_single_country('US', CONFIRMED_DATA, DATE_FIELDS, show_daily_increase=True, cases_in_log=False,
+    show_single_country('Germany', CONFIRMED_DATA, DATE_FIELDS, show_daily_increase=True, cases_in_log=False,
                         increases_in_log=False)
 
     # Compare two countries
