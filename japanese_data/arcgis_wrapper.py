83987bf259776c7a016efd9f7da11288802a96a7
Arne Hitzmann <arne.hitzmann@gmail.com>
2020-04-23 01:05:37 +0900

    first working gui for japanese data

--- japanese_data/arcgis_wrapper.py
+++ japanese_data/arcgis_wrapper.py
@@ -11,7 +11,7 @@ class ArcGisWrapper(object):
     def __init__(self, url, save_path='./', max_age_hrs=3):
         self.max_age_hrs = max_age_hrs
         data = self._get_xhr_from_link(url, save_path=save_path)
-        self.__dict__ = json.loads(data)
+        self.__dict__ = json.loads(data, parse_int=self._to_string, parse_float=self._to_string)
 
     def get_features_grouped_by_name(self, selected_feature):
         ret = dict()
@@ -23,25 +23,37 @@ class ArcGisWrapper(object):
             ret[f[selected_feature]].append(f)
         return ret
 
+    def get_fields(self):
+        ret = list()
+        for f in self.fields:
+            ret.append(f['name'])
+        return ret
+
+    # GUI only needs strings and mixups from datasource might screw with sorting functions later
+    def _to_string(self, param):
+        return str(param)
+
     def _read_file(self, raw_url):
         response = urllib.request.urlopen(raw_url)
         data = response.read()
         return data.decode('utf-8')
 
-    def _file_cache_fresh(self, file_path, max_age):
+    def _is_file_cache_fresh(self, file_path, max_age):
         last_change = os.path.getmtime(file_path)
         return time.time() < max_age + last_change
 
     def _get_xhr_from_link(self, url, save_path='./'):
         file_path = save_path + DATA_FILE_NAME
-        if os.path.isfile(file_path) and self._file_cache_fresh(file_path, SEC_PER_HR * self.max_age_hrs):
+        if os.path.isfile(file_path) and self._is_file_cache_fresh(file_path, SEC_PER_HR * self.max_age_hrs):
             body = ''
             with open(file_path, 'r') as f:
                 body = f.read()
             f.close()
-            return body
+            # Prevent NoneType ending up as keys in our dicts
+            return body.replace(':null,', ':"N.A.",')
         else:
-            body = self._read_file(url)
+            # Prevent NoneType ending up as keys in our dicts
+            body = self._read_file(url).replace(':null,', ':"N.A.",')
             with open(file_path, 'w') as f:
                 f.write(body)
             f.close()
--- japanese_data/japanese_data_gui.py
+++ japanese_data/japanese_data_gui.py
@@ -1,3 +1,8 @@
+import sys
+from builtins import map
+
+from PyQt5.QtWidgets import (QApplication, QDialog, QComboBox, QStyleFactory, QLabel, QHBoxLayout, QGridLayout,
+                             QCheckBox, QGroupBox, QTreeWidget, QBoxLayout, QTreeWidgetItem)
 from arcgis_wrapper import ArcGisWrapper
 
 DATA_URL = 'https://services8.arcgis.com/JdxivnCyd1rvJTrY/arcgis/rest/services/v2_covid19_list_csv/FeatureServer/0/'\
@@ -5,6 +10,78 @@ DATA_URL = 'https://services8.arcgis.com/JdxivnCyd1rvJTrY/arcgis/rest/services/v
            'outFields=*&orderByFields=Date%20desc%2CNo%20desc&resultOffset=0&'\
            'resultRecordCount=32000&resultType=standard&cacheHint=true'
 
-dataset = ArcGisWrapper(DATA_URL)
-sorted_case_data = dataset.get_features_grouped_by_name('Prefecture')
-pass
+
+class JapanCoronaInfoWidget(QDialog):
+    def __init__(self, parent=None):
+        super(JapanCoronaInfoWidget, self).__init__(parent)
+
+        self.dataset = ArcGisWrapper(DATA_URL)
+        self.feature_names = self.dataset.get_fields()
+
+        self.feature_checkboxes = dict()
+        self._create_feature_selection_checkbox_group()
+        self._create_treeview()
+        self._update_treeview('Prefecture_Ja')
+
+        sort_combo_box = QComboBox()
+        sort_combo_box.addItems(self.feature_names)
+
+        sort_label = QLabel('&Sort by:')
+        sort_label.setBuddy(sort_combo_box)
+
+        top_layout = QHBoxLayout()
+        top_layout.addWidget(sort_label)
+        top_layout.addWidget(sort_combo_box)
+
+        main_layout = QGridLayout()
+        main_layout.addLayout(top_layout, 0, 0)
+        main_layout.addWidget(self.feature_selection_groupbox, 1, 0)
+        main_layout.addWidget(self.data_treeview, 2, 0)
+
+        sort_combo_box.activated[str].connect(self._main_feature_changed)
+
+        self.setLayout(main_layout)
+        self.setWindowTitle('Case Information for Japan')
+
+    def _create_feature_selection_checkbox_group(self):
+        self.feature_selection_groupbox = QGroupBox('Feature selection')
+        self.feature_selection_groupbox.setCheckable(True)
+        self.feature_selection_groupbox.setChecked(False)
+
+        layout = QHBoxLayout()
+        for f in self.feature_names:
+            t = QCheckBox('{}'.format(f))
+            t.setChecked(True)
+            self.feature_checkboxes[f] = t
+            layout.addWidget(t)
+        self.feature_selection_groupbox.setLayout(layout)
+
+    def _create_treeview(self):
+        self.data_treeview = QTreeWidget()
+
+    def _main_feature_changed(self, feature_name):
+        name = feature_name
+        self._update_treeview(str(feature_name))
+
+    def _update_treeview(self, root_feature):
+        case_data = self.dataset.get_features_grouped_by_name(root_feature)
+        headers = self.feature_names.copy()
+        headers.insert(0, root_feature)
+
+        self.data_treeview.clear()
+        self.data_treeview.setHeaderLabels(headers)
+        for d in sorted(case_data):
+            tree_root = QTreeWidgetItem(self.data_treeview)
+            tree_root.setText(0, '{} ({})'.format(d, len(case_data[d])))
+            for case in case_data[d]:
+                sub_item = QTreeWidgetItem(tree_root)
+                sub_item.setText(0, str(d))
+                for column, case_item in enumerate(case):
+                    sub_item.setText(column + 1, str(case[case_item]))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    gallery = JapanCoronaInfoWidget()
+    gallery.show()
+    sys.exit(app.exec_())
